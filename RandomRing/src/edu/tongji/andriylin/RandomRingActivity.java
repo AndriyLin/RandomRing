ea1d16f1e4a89d36e52f651b090f359b005e7bce
Andriy Lin <lxk19921205@gmail.com>
2012-05-27 11:13:34 +0800

    可以根据一个uri设置为铃声了；可以从系统的铃声中选择一个铃声了

--- RandomRing/src/edu/tongji/andriylin/RandomRingActivity.java
+++ RandomRing/src/edu/tongji/andriylin/RandomRingActivity.java
@@ -1,13 +1,65 @@
 package edu.tongji.andriylin;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
+/**
+ * 主界面activity
+ * @author Andriy
+ */
 public class RandomRingActivity extends Activity {
+
+    private Button testButton1;
+    private Button testButton2;
+    
+    private static final int PICK_RINGTONE_REQUEST = 1;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        testButton1 = (Button) findViewById(R.id.button1);
+        testButton2 = (Button) findViewById(R.id.button2);
+        testButton1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+				intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_RINGTONE);
+				intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "添加到铃声库中");
+				RandomRingActivity.this.startActivityForResult(intent, PICK_RINGTONE_REQUEST);
+			}
+		});
+        testButton2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				RingtoneUtil util = new RingtoneUtil(RandomRingActivity.this);
+				Toast.makeText(RandomRingActivity.this, util.getDefaultRingtoneTitle(), Toast.LENGTH_SHORT).show();
+			}
+		});
     }
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == PICK_RINGTONE_REQUEST) {
+			if (resultCode == RESULT_OK) {
+				Uri uri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
+				RingtoneUtil util = new RingtoneUtil(RandomRingActivity.this);
+				util.setRingtone(uri);
+			}
+			return;
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+    
 }
---
+++ RandomRing/src/edu/tongji/andriylin/RingtoneUtil.java
@@ -0,0 +1,31 @@
+package edu.tongji.andriylin;
+
+import android.content.Context;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+
+/**
+ * 与ringtone相关的设置
+ * @author Andriy
+ */
+public class RingtoneUtil {
+	
+	private final Context context;
+	public RingtoneUtil(Context aContext) {
+		this.context = aContext;
+	}
+	
+	public String getDefaultRingtoneTitle() {
+		Uri uri = RingtoneManager.getActualDefaultRingtoneUri(this.context, RingtoneManager.TYPE_RINGTONE);
+		Ringtone ringtone = RingtoneManager.getRingtone(this.context, uri);
+		return ringtone.getTitle(this.context);
+	}
+
+	/**
+	 * 设置一首歌为当前铃声
+	 */
+	public void setRingtone(Uri uri) {
+		RingtoneManager.setActualDefaultRingtoneUri(this.context, RingtoneManager.TYPE_RINGTONE, uri);			
+	}
+}
