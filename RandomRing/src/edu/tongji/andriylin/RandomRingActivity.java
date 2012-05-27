47a387b1d111275ad6b7b34a458598ee3215fa39
Andriy Lin <lxk19921205@gmail.com>
2012-05-27 09:23:20 +0800

    first init

---
+++ RandomRing/src/edu/tongji/andriylin/RandomRingActivity.java
@@ -0,0 +1,13 @@
+package edu.tongji.andriylin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RandomRingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
